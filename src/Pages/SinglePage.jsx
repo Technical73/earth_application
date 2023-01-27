16acd3767edeb6e2a8b73b981270b01ff7e19b99
Technical73 <w50790679@gmail.com>
2023-01-27 22:43:36 +0530

    I have prepared everything for an project

---
+++ src/Pages/SinglePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../components";
+
+const SinglePage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default SinglePage;
