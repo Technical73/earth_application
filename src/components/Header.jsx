d53005572587e73fcea4f334d0d6ff893aae130c
Technical73 <w50790679@gmail.com>
2023-01-28 16:30:53 +0530

    Finally I Have found everythings.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,49 @@
 import React from "react";
-
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../assets/logoEB.96f33359 (1).png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Container
+        fluid
+        style={{
+          backgroundColor: "rgb(12, 3, 56)",
+        }}
+      >
+        <Navbar
+          expand="lg"
+          className=" bg-transparent"
+          variant="dark"
+          style={{
+            padding: "16px 10px",
+            fontWeight: "bold",
+            color: "white",
+          }}
+        >
+          <Container>
+            <Navbar.Brand>
+              <img
+                src={`${logo}`}
+                alt="logo"
+                width="200"
+                height="50"
+                className="d-inline-block align-top"
+              ></img>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+              <Nav className="ms-auto">
+                <Nav.Link>COLLECTION</Nav.Link>
+                <Nav.Link>SOIL CRISES</Nav.Link>
+                <Nav.Link>#SAVESOIL</Nav.Link>
+                <Nav.Link>FAQS</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+    </>
+  );
 };
 
 export default Header;
